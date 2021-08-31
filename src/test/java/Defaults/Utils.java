92b17cee76e44345248e6ae9249b351d65c5c38f
Anthony Cholakov <antoni.cholakov.123@gmail.com>
2021-08-31 23:26:36 +0300

    Initial commit

---
+++ src/test/java/Defaults/Utils.java
@@ -0,0 +1,10 @@
+package Defaults;
+import Tests.BaseTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Utils{
+    public static final String BASE_URL = "https://localhost:44351/grid";
+}
