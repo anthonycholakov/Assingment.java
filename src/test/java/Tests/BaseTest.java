be882ae287782ba3176a55e7f7b90e684e8f41b1
Anthony Cholakov <antoni.cholakov.123@gmail.com>
2021-09-02 11:01:55 +0300

    this is an example

--- src/test/java/Tests/BaseTest.java
+++ src/test/java/Tests/BaseTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 
 import java.util.concurrent.TimeUnit;
